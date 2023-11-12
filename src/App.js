e40addadbbc9b1bc446e06070a54ed7214829447
jakar79 <karim.jabri@gmail.com>
2023-11-12 22:23:47 +0100

    committed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,66 @@ import './App.css';
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {} from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck, faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+
+  //Task Stat
+  const [toDo, setToDo] = useState([
+    {id:1, title: "Task1", status: false},
+    {id:2, title: "Task2", status: false}
+  ]);
+
+  //Temp Stat
+  const [newTask, setNewTask] = useState('');
+  const [updateData, setUpdateData] = useState('');
+
+  // Add Task
+  const addTask = () => {
+
+  };
+
+  // Delete Task
+
+  const deleteTask = (id) => {
+    
+  };
+
+  // Mark task completed
+
+  const taskComleted = (id) => {
+    
+  };
+
+  //Cancel Update
+
+  const cancelUpdate = () => {
+    
+  };
+
+  //Change Task for update
+
+  const changeTask = (e) => {
+    
+  };
+
+  // Update Task
+
+  const updateTask = () => {
+    
+  };
+
+
   return (
-    <div className="App">
-      webstylePress..
+    <div className="container App">
+      <br/><br/>
+      <h2>To Do List App</h2>
+
+      {/* Display ToDos */}
+
+      { toDo && toDo.length ? '' : 'No Task...' }
+
+
     </div>
   );
 }
